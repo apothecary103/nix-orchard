e8eea0fb9faf7e49cb69986ee7bf8dafffb3a36c
apothecary <113787039+apothecary103@users.noreply.github.com>
2026-08-21 21:14:33 +0300

    chore: add flake scaffolding

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Orchard: hand-crafted themes as home-manager, hjem and NixOS modules";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+
+      engine = import ./lib { inherit lib; };
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      modulesFor = class: rec {
+        orchard = engine.mkModule class;
+        default = orchard;
+      };
+    in
+    {
+      lib = engine;
+
+      inherit (engine) themes ports;
+
+      # theme -> flavor -> the resolved palette, on that theme's default accent.
+      palettes = lib.mapAttrs (
+        name: theme:
+        lib.genAttrs (engine.palette.flavorsOf theme) (
+          flavor:
+          engine.mkPalette name {
+            inherit flavor;
+            accent = theme.defaultAccent;
+          }
+        )
+      ) engine.themes;
+
+      hjemModules = modulesFor "hjem";
+      homeModules = modulesFor "home";
+      nixosModules = modulesFor "nixos";
+
+      checks = lib.genAttrs systems (
+        system:
+        import ./tests {
+          inherit lib engine;
+          pkgs = nixpkgs.legacyPackages.${system};
+        }
+      );
+
+      formatter = lib.genAttrs systems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+    };
+}
